048a10cb7cc299b4ff545bebe217248bd2e9c4d1
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-10-01 16:58:07 +0530

    Button pin and timing variables

--- traffic.ino
+++ traffic.ino
@@ -9,6 +9,15 @@ const int redVehiclePin = 10;
 const int yellowVehiclePin = 11;
 const int greenVehiclePin = 12;
 
+// Button pin and timing variables
+const int buttonPin = 2;
+const unsigned long buttonDebounceDelay = 50; // Debounce delay in milliseconds
+const unsigned long vehicleGreenToYellowDelay = 2000; // Delay before changing vehicle light to yellow
+const unsigned long yellowBlinkInterval = 1000; // Yellow blink interval in milliseconds
+const unsigned long yellowBlinkDuration = 3000; // Total yellow blink duration in milliseconds
+const unsigned long pedestrianCrossDuration = 5000; // Duration pedestrian light is green
+
+
 void setup() {
   // put your setup code here, to run once:
    
