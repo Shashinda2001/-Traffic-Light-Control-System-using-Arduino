848d79c3092cac981fd1fa62e52b81ae361445da
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-10-01 16:51:07 +0530

    Pin definitions for pedestrian path

--- traffic.ino
+++ traffic.ino
@@ -1,9 +1,22 @@
+
+// Pin definitions for pedestrian path
+const int redPedestrianPin = 8;
+const int greenPedestrianPin = 9;
+
 void setup() {
   // put your setup code here, to run once:
-//hhhhhhhhhhh
+   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+//
+//Pedestrian Path-Red and Green lights. Initially,
+//Red is ON. After the button is pressed, the Red turns 
+//OFF and Green turns ON after 6 seconds. ,Vehicle Path-Red,
+//Yellow, Green lights. Initially, Green is ON. After the button
+//is pressed, Green turns to Yellow after 2 seconds, Yellow blinks
+//for 3 seconds, then the pedestrian Green turns ON.
